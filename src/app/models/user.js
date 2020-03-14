22158bd81a99172973a642e25b704b2a3558a23c
samuel <samuel.silv.carvalho@gmail.com>
2020-03-14 01:32:19 -0300

    hash da senha

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,17 +1,28 @@
-import Sequelize,  { Model } from "sequelize";
+import Sequelize, { Model, useInflection } from "sequelize";
+import bcrypt from "bcryptjs";
 
-class User extends Model{
-    static init(sequelize){
+class User extends Model {
+    static init(sequelize) {
         super.init({
             name: Sequelize.STRING,
             email: Sequelize.STRING,
             password_hash: Sequelize.STRING,
+            password: Sequelize.VIRTUAL,
             provider: Sequelize.BOOLEAN,
         },
         {
             sequelize
-        })
+        });
+
+        this.addHook('beforeSave', async (user) => {
+            if (user.password)
+                user.password_hash = await bcrypt.hash(user.password, 8);
+
+            return this;
+        });
+
     }
+
 }
 
 export default User;
